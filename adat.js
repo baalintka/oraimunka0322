0a4bb6e2a1359f11d6a619055ecf14e9d97cc430
baalintka <ballint3@gmail.com>
2023-03-22 11:13:25 +0100

    rendezgetések

---
+++ adat.js
@@ -0,0 +1,10 @@
+export const SZAMLISTA =[23,5,55,11,];
+export const SZOVEGLISTA =["beka","alma","korte","cedula"];
+
+export const OBJEKTUMLISTA=[
+    {nev :"dÉZI", fajta:"keverék", kor:2},
+    {nev:"Bodri", fajta:"mudi", kor:12},
+    {nev:"Loki", fajta:"tacskó", kor:5},
+    {nev:"zuri", fajta:"foxterrier", kor:4},
+    {nev:"happy", fajta:"németjuhász", kor:42},
+    ]
---
+++ script.js
@@ -0,0 +1,57 @@
+import { SZAMLISTA, SZOVEGLISTA,OBJEKTUMLISTA } from "./adat.js";
+
+
+$(function(){
+    console.log(SZOVEGLISTA);
+    rendezesSzoveg(SZOVEGLISTA);
+    console.log(SZOVEGLISTA);
+    console.log("*********");
+    console.log(SZAMLISTA);
+    rendezesSzam(SZAMLISTA);
+    console.log(SZAMLISTA);
+    console.log("*********");
+    veletlenSorrend(SZAMLISTA);
+    console.log(SZAMLISTA);
+    console.log("*********");
+    console.log(OBJEKTUMLISTA);
+    rendezesKorszerint(OBJEKTUMLISTA);
+    console.log(OBJEKTUMLISTA);
+    console.log("*********");
+    console.log(OBJEKTUMLISTA);
+    rendezesNevszerint(OBJEKTUMLISTA);
+    console.log(OBJEKTUMLISTA);
+    console.log("*********");
+
+
+});
+function rendezesSzoveg(lista){
+    lista.sort();
+}
+function rendezesSzam(lista){
+    lista.sort(function(x,y){
+        console.log(x,y,x-y);
+        return x-y;
+    });
+}
+function veletlenSorrend(lista){
+    lista.sort(function(x,y){
+
+        return Math.random();
+    });
+}
+function rendezesKorszerint(lista){
+    lista.sort(function(a,b){
+        console.log(a,b);
+        return a.kor - b.kor;
+    });
+function rendezesNevszerint(lista,kulcs){
+    lista.sort(function(a,b){
+    
+        if (a[kulcs]>b[kulcs]) {
+            return 1;
+        }else{
+            return -1;
+        }
+        });
+    }
+}
